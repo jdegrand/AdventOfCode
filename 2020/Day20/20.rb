2181b0b3889fed7011ce49ad2f7fc2b6f514153e
Joe DeGrand <degrandjoe@gmail.com>
2021-11-05 20:20:59 -0400

    Not the best Day 20 solution  but it works

--- 2020/Day20/20.rb
+++ 2020/Day20/20.rb
@@ -5,7 +5,6 @@ input = File.read(file)
 
 $lines = input.split("\n\n")
 
-
 #     #.#.#####.
 #     .#..######
 #     ..#.......
@@ -18,19 +17,9 @@ $lines = input.split("\n\n")
 #     ..#.###...
 
 class Tile
-    def initialize(id, board)
-        @id = id
+    def initialize(tid, board)
+        @tid = tid
         @board = board
-        @position = -1
-        @coming_from = [-1, -1] #id, direction
-    end
-
-    def setPosition(p)
-        @position = p
-    end
-
-    def setComingFrom(p)
-        @coming_from = p
     end
 
     def getEdges
@@ -42,145 +31,28 @@ class Tile
         edges += edges.map{|e| e.reverse}
     end
 
-    def getSplitEdges
-        edges1 = []
-        edges1 << [@board[0], :t]
-        edges1 << [@board[-1], :b]
-        edges1 << [@board.map{|r| r[0]}.join, :l]
-        edges1 << [@board.map{|r| r[-1]}.join, :r]
-
-        edges2 = []
-        edges2 << [@board[0], :t]
-        edges2 << [@board[-1], :b]
-        edges2 << [@board.map{|r| r[0]}.join.reverse, :r]
-        edges2 << [@board.map{|r| r[-1]}.join.reverse, :l]
-
-        edges3 = []
-        edges3 << [@board[0].reverse, :b]
-        edges3 << [@board[-1].reverse, :t]
-        edges3 << [@board.map{|r| r[0]}.join, :l]
-        edges3 << [@board.map{|r| r[-1]}.join, :r]
-
-        [edges1, edges2, edges3]
+    def arrayCombinations
+        combs = []
+        curr = @board.map{|r| r.split("")}
+        4.times do
+            combs << Tile.new(@tid, curr.map(&:join))
+            curr = curr.transpose.map(&:reverse)
+        end
+        curr = curr.map{|r| r.reverse}
+        4.times do
+            combs << Tile.new(@tid, curr.map(&:join))
+            curr = curr.transpose.map(&:reverse)
+        end
+        @combinations = combs
     end
 
-    attr_accessor :id
+    def transposeBoard
+        @board = @board.map{|r| r.split("")}.transpose.map(&:join)
+    end
+
+    attr_accessor :tid
     attr_accessor :board
-    attr_accessor :coming_from
-end
-
-def dfs(tiles_by_id, tiles, edges)
-    stack = []
-    visited = Set.new
-    stack.push(tiles[0])
-    while !stack.empty?
-        t = stack.pop
-        if !visited.include?(t.id)
-            visited << t.id
-            all_var = []
-            splits = t.getSplitEdges
-            splits.length.times do |i|
-                se = splits[i]
-                temp = Set.new
-                se.each do |e|
-                    edges[e[0]].reject{|v| v == t.id}.each do |d|
-                        temp << [d, e[1]]
-                    end
-                end
-                all_var << [temp.count, temp, i + 1]
-            end
-            max = all_var.max_by{|x, _, _| x}
-            max[1].each do |tid|
-                tile = tiles_by_id[tid[0]]
-                dir = case tid[1]
-                        when :r
-                            :l
-                        when :l
-                            :r
-                        when :t
-                            :b
-                        when :b
-                            :t
-                        end
-                tile.setComingFrom([t.id, dir])
-                tile.setPosition(max[2])
-                stack << tile
-            end
-        end
-    end
-    tiles
-end
-
-def arrayCombinations(tile)
-    combs = Set.new
-    curr = tile.board.map{|r| r.split("")}
-    4.times do
-        combs << curr.map(&:join)
-        curr = curr.transpose.map(&:reverse)
-    end
-    curr = curr.map{|r| r.reverse}
-    4.times do
-        combs << curr.map(&:join)
-        curr = curr.transpose.map(&:reverse)
-    end
-    combs
-end
-
-def day20_2
-    visited = Set.new
-    tile_list = []
-    tiles = {}
-    edges = Hash.new(Set.new)
-    $lines.each do |l|
-        sp = l.split("\n")
-        id = sp[0].match(/\ATile (\d+):\Z/).captures.map(&:to_i)[0]
-        tile = Tile.new(id, sp[1..])
-        tiles[id] = tile
-        tile_list << tile
-        tile.getSplitEdges.flatten.each do |e|
-            edges[e] += [id]
-        end
-    end
-    dfs(tiles, tile_list, edges)
-    x = 0
-    y = 0
-    grid = {}
-    grid[[x,y]] = tile_list[0]
-    tile_list[1..].each do |t|
-        case t.coming_from[1]
-        when :r
-            x += 1
-        when :l
-            x -= 1
-        when :t
-            y += 1
-        when :b
-            x -= 1
-        end
-        grid[[x,y]] = t
-    end
-    # pp grid
-    arrayCombinations(tile_list[0])
-    # min_x = grid.keys.min_by{|x, _| x}[0]
-    # min_y = grid.keys.min_by{|_, y| y}[1]
-    # size = Math.sqrt(grid.length)
-    # (min_x...(min_x+size)).to_a.each do |dx|
-    #     10.times do |t|
-    #         s = ""
-    #         (min_y...(min_y+size)).to_a.each do |dy|
-    #             pp dx
-    #             pp dy
-    #             pp grid
-    #             s += grid[[dx,dy]].board[t]
-    #         end
-    #         puts t
-    #     end
-    # end
-
-    # tile_list[0]
-    # edges
-    # filtered = edges.reject{|e| e.length < 2}
-    # tile_list.map{ |t| [t.getEdges.count{|e| filtered[e].length > 1}, t.id] }.sort_by{|x, _| x}[0...4].map{|_, id| id}.reduce(:*)
+    attr_accessor :combinations
 end
 
 def day20_1
@@ -197,9 +69,112 @@ def day20_1
         end
     end
     filtered = edges.reject{|e| e.length < 2}
-    tile_list.map{ |t| [t.getEdges.count{|e| filtered[e].length > 1}, t.id] }.sort_by{|x, _| x}[0...4].map{|_, id| id}.reduce(:*)
+    tile_list.map{ |t| [t.getEdges.count{|e| filtered[e].length > 1}, t.tid] }.sort_by{|x, _| x}[0...4].map{|_, tid| tid}.reduce(:*)
+end
+
+def checkValid(c, row, col, grid, emptyTile)
+    topValid = false
+    leftValid = false
+    if row == 0 && col == 0 then return true end
+    if row == 0
+        topValid = true
+    else
+        topValid = grid[[row - 1, col]].board[-1] == c.board[0]
+    end
+    if col == 0
+        leftValid = true
+    else
+        leftValid = grid[[row, col - 1]].board.map{|r| r[-1]}.join == c.board.map{|r| r[0]}.join
+    end
+    return topValid && leftValid
+end
+
+def solveBoard(row, col, tile_list, visited_ref, grid, size, emptyTile, count)
+    if row == size
+        return true
+    end
+    visited = visited_ref.map(&:clone)
+    tile_list.length.times do |i|
+        t = tile_list[i]
+        if !visited.include?(t.tid)
+            visited += [t.tid]
+            t.combinations.each do |c|
+                if checkValid(c, row, col, grid, emptyTile)
+                    grid[[row, col]] = c
+                    if col + 1 == size
+                        if solveBoard(row + 1, 0, tile_list, visited, grid, size, emptyTile, count+1) == true then return true end
+                    else
+                        if solveBoard(row, col + 1, tile_list, visited, grid, size, emptyTile, count+1) == true then return true end
+                    end
+                end
+            end
+            grid[[row, col]] = emptyTile
+            visited -= [t.tid]
+        end
+    end
+end
+
+def getGrid(grid, size)
+    conjoined = []
+    size.to_i.times do |c|
+        8.times do |t|
+            s = ""     
+            size.to_i.times do |r|
+                s += grid[[r,c]].board[t+1][1...-1]
+            end
+            conjoined << s
+        end
+    end
+    conjoined
+end
+
+def day20_2
+    tile_list = []
+    edges = Hash.new([])
+    $lines.each do |l|
+        sp = l.split("\n")
+        id = sp[0].match(/\ATile (\d+):\Z/).captures.map(&:to_i)[0]
+        tile = Tile.new(id, sp[1..])
+        tile_list << tile
+        tile.getEdges.each do |e|
+            edges[e] += [id]
+        end
+    end
+    filtered = edges.reject{|e| e.length < 2}
+    corner = tile_list.map{ |t| [t.getEdges.count{|e| filtered[e].length > 1}, t.tid] }.sort_by{|x, _| x}[0][1]
+    corner = tile_list.detect{|t| t.tid == corner}
+    tile_list.each{|t| t.arrayCombinations}
+    visited = Set.new
+    emptyTile = Tile.new(-1, [""])
+    grid = Hash.new(emptyTile)
+    size = Math.sqrt(tile_list.length)
+    tile_list = tile_list.reject{|t| t.tid == corner.tid}.unshift(corner)
+    visited = []
+    solveBoard(0, 0, tile_list, visited, grid, size, emptyTile, 0)
+    grid.map{|_, t| t.transposeBoard}
+    image = getGrid(grid, size)
+    sea_monster_1 = Regexp.new("\\A                  # ".gsub(" ", "."))
+    sea_monster_2 = Regexp.new("\\A#    ##    ##    ###".gsub(" ", "."))
+    sea_monster_3 = Regexp.new("\\A #  #  #  #  #  #   ".gsub(" ", "."))
+    counts = []
+    image_tile = Tile.new(2020, image)
+    image_tile.arrayCombinations
+    image_tile.combinations.each do |c|
+        count = 0
+        (c.board.length - 2).times do |r|
+            (c.board[r].length - 10).times do |t|
+                ind1 = c.board[r][t..].index(sea_monster_1)
+                ind2 = c.board[r+1][t..].index(sea_monster_2)
+                ind3 = c.board[r+2][t..].index(sea_monster_3)
+                if ind1 && ind1 == ind2 && ind1 == ind3
+                    count += 1
+                end
+            end
+        end
+        counts << count
+    end
+    image.join.count("#") - (counts.max * 15)
 end
 
 pp day20_1
 pp day20_2
-
