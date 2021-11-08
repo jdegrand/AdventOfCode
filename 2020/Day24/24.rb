42ac7029a56b9f6208596e0997e216ccfd26f0de
Joe DeGrand <degrandjoe@gmail.com>
2021-11-07 22:50:33 -0500

    Day 24 Complete

--- 2020/Day24/24.rb
+++ 2020/Day24/24.rb
@@ -20,51 +20,58 @@ def day24_1
     tiles = Hash.new(:white)
     reg = /(ne|nw|se|sw|e|w)/
     $lines.each do |l|
-        x, y = 0, 0
+        x, y, z = 0, 0, 0
         l.scan(reg).flatten.each do |d|
             case d
             when "e"
-                x += 0.5
-                y += 0.5
+                x += 1
+                z -= 1
             when "w"
-                x -= 0.5
-                y -= 0.5
+                x -= 1
+                z += 1
             when "ne"
                 y += 1
+                z -= 1
             when "sw"
                 y -= 1
+                z += 1
             when "nw"
-                x -= 0.5
-                y += 0.5
+                x -= 1
+                y += 1
             when "se"
-                x += 0.5
-                y -= 0.5
+                x += 1
+                y -= 1
             end
         end
-        tiles[[x, y]] = tiles[[x, y]] == :white ? :black : :white
+        tiles[[x, y, z]] = tiles[[x, y, z]] == :white ? :black : :white
     end
     tiles
 end
 
 def day24_2
     tiles = day24_1
-    surrounding = [0.5, -0.5].product([-0.5, 0.5]) + [[0, 1]] + [[0, -1]]
-    pp surrounding
+    surrounding = [[1, 0, -1], [-1, 0, 1], [0, 1, -1], [0, -1, 1], [-1, 1, 0], [1, -1, 0]]
     100.times do |day|
         new_tiles = tiles.clone
         tiles.each do |k, v|
-            neighbors = surrounding.map{|dx, dy| [k[0] + dx, k[1] + dy]}.reject{|t| !tiles.include?(t)}
-            pp neighbors
-            neighbors = neighbors.map{|n| tiles[n]}
-            b = neighbors.count(:black)
+            neighbors = surrounding.map{|dx, dy, dz| [k[0] + dx, k[1] + dy, k[2] + dz]}
+            neighbors = neighbors.map{|n| [n, tiles[n]]}
+            b = neighbors.count{|c, val| val == :black}
             if v == :black && (b == 0 || b > 2) then new_tiles[k] = :white end
             if v == :white && (b == 2) then new_tiles[k] = :black end
+            neighbors.each do |c, col|
+                new_neighbors = surrounding.map{|dx, dy, dz| [c[0] + dx, c[1] + dy, c[2] + dz]}
+                new_neighbors = new_neighbors.map{|n| tiles[n]}
+                sb = new_neighbors.count(:black)
+                if col == :black && (sb == 0 || sb > 2) then new_tiles[c] = :white end
+                if col == :white && (sb == 2) then new_tiles[c] = :black end
+            end
         end
         tiles = new_tiles
-        counts = tiles.values.count{|v| v == :black}
-        pp "Day: #{day + 1}  -  #{counts} Black Tiles"
+        # counts = new_tiles.values.count{|v| v == :black}
+        # pp "Day: #{day + 1}  -  #{counts} Black Tiles"
     end
-
+    tiles.values.count{|v| v == :black}
 end
 
 puts day24_1.values.count{|v| v == :black}
