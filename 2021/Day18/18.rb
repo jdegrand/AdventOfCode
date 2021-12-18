e28628f1f8223638c04137e9516530ea2b340bda
Joe DeGrand <degrandjoe@gmail.com>
2021-12-18 17:26:48 -0500

    Rough solution for Day 18

--- 2021/Day18/18.rb
+++ 2021/Day18/18.rb
@@ -3,58 +3,42 @@ input = File.read(file)
 
 $lines = input.lines.map(&:chomp)
 
-def explode(nums, l, r, i, loc)
-    if loc == :right    # Pair is on right
-        # [7,[6,[5,[4,[3,2]]]]]
-        if i + 2 < nums.length
-            to_change, to_change_depth = nums[i + 2]
-            nums[i + 2] = [r[0] + to_change, to_change_depth]
-        end
-        new_l = l[0] + nums[i - 1][0]
-        nums[i - 1] = [new_l, nums[i - 1][1]]
-        nums[i + 1] = [0, r[1] - 1]
-        nums.delete_at(i)
-    elsif loc == :left   # Pair is on left
-        # [[[[[9,8],1],2],3],4]
-        if i > 0
-            to_change, to_change_depth = nums[i - 1]
-            nums[i - 1] = [l[0] + to_change, to_change_depth]
-        end
-        new_r = r[0] + nums[i + 2][0]
-        nums[i] = [0, l[1] - 1]
-        nums[i + 2] = [new_r,  nums[i + 2][1]]
-        nums.delete_at(i + 1)
+def explode(nums, l, r, i)
+    if i > 0
+        to_change, to_change_depth = nums[i - 1]
+        nums[i - 1] = [l[0] + to_change, to_change_depth]
     end
+    if i + 2 < nums.length
+        to_change, to_change_depth = nums[i + 2]
+        nums[i + 2] = [r[0] + to_change, to_change_depth]
+    end
+    nums[i] = [0, l[1] - 1]
+    nums.delete_at(i + 1)
 end
 
 def reduce(nums)
     nums.each_cons(2).to_a.each_with_index do |(l, r), i|
-        pp "#{l}, #{r}"
+        if l[1] == 5 && r[1] == 5
+            explode(nums, l, r, i)
+            return
+        end
+    end
+    nums.each_cons(2).to_a.each_with_index do |(l, r), i|
         if l[0] >= 10
             new_l = [(l[0]/2.0).floor, l[1] + 1]
             new_r = [(l[0]/2.0).ceil, l[1] + 1]
-            new_d = l[1] + 1
             nums[i] = new_l
             nums.insert(i + 1, new_r)
-            explode(nums, new_l, new_r, i, :left) if new_d > 4
             return
-        elsif r[0] >= 10
-            new_l = [(r[0]/2.0).floor, r[1] + 1]
-            new_r = [(r[0]/2.0).ceil, r[1] + 1]
-            new_d = r[1] + 1
-            nums[i + 1] = new_l
-            nums.insert(i + 2, new_r)
-            explode(nums, new_l, new_r, i + 1, :right) if new_d > 4
-            return
-        elsif l[1] == 5 && r[1] == 5
-            if i > 0 && nums[i - 1][1] >= 4     # Pair is on right
-                explode(nums, l, r, i, :right)
-                return
-            elsif i < nums.length && nums[i + 2][1] >= 4 # Pair is on left
-                explode(nums, l, r, i, :left)
-                return
-            end
         end
+    end
+    if nums[-1][0] >= 10
+        r = nums[-1]
+        new_l = [(r[0]/2.0).floor, r[1] + 1]
+        new_r = [(r[0]/2.0).ceil, r[1] + 1]
+        nums[-1] = new_l
+        nums << new_r
+        return
     end
 end
 
@@ -90,11 +74,11 @@ end
 
 def reduceAll(nums)
     changed = true
-    last_length = nums.length
+    last_nums = nums.map(&:clone)
     while changed
         reduce(nums)
-        changed = last_length != nums.length
-        last_length = nums.length
+        changed = last_nums != nums
+        last_nums = nums.map(&:clone)
     end
 end
 
@@ -112,14 +96,29 @@ def day18_1
         nums = last_nums.map{|n, d| [n, d + 1]} + nums.map{|n, d| [n, d + 1]}
         reduceAll(nums)
     end
-    pp nums
     while nums.length > 1
         magnitude(nums)
     end
-    nums
+    nums[0][0]
 end
 
 def day18_2
+    mags = []
+    $lines.permutation(2).each do |l1, l2|
+        nums1 = []
+        string_to_nums(nums1, l1)
+        reduceAll(nums1)
+        nums2 = []
+        string_to_nums(nums2, l2)
+        reduceAll(nums2)
+        nums = nums1.map{|n, d| [n, d + 1]} + nums2.map{|n, d| [n, d + 1]}
+        reduceAll(nums)
+        while nums.length > 1
+            magnitude(nums)
+        end
+        mags << nums[0][0]
+    end
+    mags.max
 end
 
 pp day18_1
