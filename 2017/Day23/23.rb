18655771e9add4661d88e0f316c60586bcc7bd3e
Joe DeGrand <degrandjoe@gmail.com>
2022-11-29 18:58:01 -0800

    Day 23 2017 Part 2 Started

--- 2017/Day23/23.rb
+++ 2017/Day23/23.rb
@@ -34,36 +34,42 @@ end
 
 
 def day23_2
-    registers = Hash.new(0)
-    registers[?a] = 1
-    instruction = 0
-    mul_count = 0
-    until instruction >= $lines.size
-        op, p1, p2 = $lines[instruction].split
-        p1_is_int = /\A[-+]?\d+\z/.match(p1)
-        p2 = /\A[-+]?\d+\z/.match(p2) ? p2.to_i : registers[p2]
-        case op
-        when 'set'
-            registers[p1] = p2
-        when 'sub'
-            registers[p1] -= p2
-        when 'mul'
-            mul_count += 1
-            registers[p1] *= p2
-        when 'mod'
-            registers[p1] %= p2
-        when 'jnz'
-            # pp p1
-            # pp (p1_is_int ? p1.to_i : registers[p1])
-            instruction += (p1_is_int ? p1.to_i : registers[p1]) != 0 ? p2 : 1
-            next
-        else
-            raise 'Double parameters not matched!'
+    a, b, c, d, e, f, g, h = 1, 0, 0, 0, 0, 0, 0, 0
+    b = 105700
+    c = 122700
+    loop do
+        f = 1
+        d = 2
+        loop do
+            e = 2
+            broken = loop do
+                g = (d * e) - b
+                if g == 0
+                    f = 0
+                    break :broke
+                end
+                e += 1
+                g = e - b
+                if g == 0
+                    break
+                end
+            end
+            break if broken == :broke
+            d += 1
+            g = d - b
+            if g == 0
+                break
+            end
         end
-        instruction += 1
-        # pp instruction
+        if f == 0
+            h += 1
+        end
+        g = b - c
+        if g == 0
+            break
+        end
+        b += 17
     end
-    mul_count
 end
 
 pp day23_1
