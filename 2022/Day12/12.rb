7844f6b9baa3f329ee748256170be1e5a5f16cb0
Joe DeGrand <degrandjoe@gmail.com>
2022-12-11 22:12:50 -0800

    Day 12 2022

--- 2022/Day12/12.rb
+++ 2022/Day12/12.rb
@@ -5,14 +5,15 @@ input = File.read(file)
 
 $lines = input.lines.map(&:chomp)
 
-def backtack(curr, steps, visited, grid, ways)
+def backtack(curr, steps, visited, grid, ways, dp)
     ways << steps if grid[curr] == ?|
+    dp[curr] = steps
 
     [[1, 0], [-1, 0], [0, 1], [0, -1]].each do |dx, dy|
         coord = [curr[0] + dx, curr[1] + dy]
-        if !visited.include?(coord) && ((grid[coord].ord <= (grid[curr].ord + 1)) ||  (grid[curr] == ?z && grid[coord] == ?|))
+        if !visited.include?(coord) && dp[coord] > steps + 1 && ((grid[coord].ord <= (grid[curr].ord + 1)) ||  (grid[curr] == ?z && grid[coord] == ?|))
             visited << coord
-            backtack(coord, steps + 1, visited, grid, ways)
+            backtack(coord, steps + 1, visited, grid, ways, dp)
             visited.delete(coord)
         end
     end
@@ -21,38 +22,52 @@ end
 def day12_1
     grid = Hash.new(?~)
     start = nil
-    en = nil
+    goal = nil
     $lines.each_with_index do |l, ri|
         l.chars.each_with_index do |c, ci|
             grid[[ri, ci]] = c
             start = [ri, ci] if c == ?S
-            en = [ri, ci] if c == ?E
+            goal = [ri, ci] if c == ?E
         end
     end
-    visited = Set.new
-    queue = []
-    queue << [start, 0]
+
     grid[start] = ?a
-    grid[en] = ?|
+    grid[goal] = ?|
+
+    visited = Set.new
+    dp = Hash.new(Float::INFINITY)
     ways = []
-    visited << start
-    # backtack(start, 0, visited, grid, ways)
-    until queue.empty?
-        curr, steps = queue.shift
-        ways << steps if grid[curr] == ?|
-        return steps if grid[curr] == ?|
-        visited << curr
-        to_visit = []
-        [[1, 0], [-1, 0], [0, 1], [0, -1]].each do |dx, dy|
-            coord = [curr[0] + dx, curr[1] + dy]
-            to_visit << [coord, steps + 1] if !visited.include?(coord) && ((grid[coord].ord <= (grid[curr].ord + 1)) ||  (grid[curr] == ?z && grid[coord] == ?|))
-        end
-        to_visit.sort{|coord, _| (grid[curr].ord + 1) - grid[coord].ord}.each{|coord| queue << coord}
-    end
-    pp ways.min
+
+    backtack(start, 0, visited, grid, ways, dp)
+    
+    ways.min
 end
 
 def day12_2
+    grid = Hash.new(?~)
+    starts = []
+    goal = nil
+    $lines.each_with_index do |l, ri|
+        l.chars.each_with_index do |c, ci|
+            grid[[ri, ci]] = c
+            if c == ?S
+                grid[[ri, ci]] = ?a
+                starts << [ri, ci]
+            elsif c == ?a
+                starts << [ri, ci]
+            end
+            goal = [ri, ci] if c == ?E
+        end
+    end
+
+    grid[goal] = ?|
+
+    visited = Set.new
+    dp = Hash.new(Float::INFINITY)
+    ways = []
+    starts.each{|start| backtack(start, 0, visited, grid, ways, dp)}
+    
+    ways.min
 end
 
 pp day12_1
