f48db4ca003edd75737aa6b3e10f61a534a57550
Joe DeGrand <degrandjoe@gmail.com>
2022-12-16 00:11:57 -0800

    Day 15 2022

--- 2022/Day15/15.rb
+++ 2022/Day15/15.rb
@@ -6,7 +6,8 @@ input = File.read(file)
 $lines = input.lines.map(&:chomp)
 
 def day15_1
-    rows = Hash.new(Set.new)
+    row_to_check = 2000000
+    rows = Hash.new{[]}
     beacons = Set.new
     scanners = Set.new
     $lines.each do |l|
@@ -14,23 +15,64 @@ def day15_1
         scanners << [sx, sy]
         beacons << [bx, by]
         manhat = (sx - bx).abs + (sy - by).abs
-        # if l == 'Sensor at x=8, y=7: closest beacon is at x=2, y=10'
-            ((sy - manhat)..(sy + manhat)).each do |lsy|
-                dx = manhat - lsy.abs
-                rows[lsy] <<= ((sx - dx)..(sx + dx))
-                # ((sx - dx)..(sx + dx)).each do |lsx|
-                #     rows[]
-                # end
-            end
-        # end
+        ((sy - manhat)..sy).each_with_index do |y, i|
+            rows[y] <<= [(sx - i), (sx + i)] if y == row_to_check
+        end
+        (sy..(sy + manhat)).each_with_index do |y, i|
+            rows[y] <<= [(sx - (manhat  - i)), (sx + (manhat - i))] if y == row_to_check
+        end
     end
     row_set = Set.new
-    rows[10].each{|range| range.each{|x| row_set << x}}
-    pp row_set
-    row_set.size - scanners.filter{|_, y| y == 10}.size - beacons.filter{|_, y| y == 10}.size
+    rows[row_to_check].each{|(l, r)| [*(l..r)].each{|n| row_set << n}}
+    row_set.size - scanners.filter{|_, y| y == row_to_check}.size - beacons.filter{|_, y| y == row_to_check}.size
 end
 
 def day15_2
+    rows = Hash.new{[]}
+    beacons = Set.new
+    scanners = Set.new
+    $lines.each do |l|
+        sx, sy, bx, by = l.scan(/-?\d+/).map(&:to_i)
+        scanners << [sx, sy]
+        beacons << [bx, by]
+        manhat = (sx - bx).abs + (sy - by).abs
+        ((sy - manhat)..sy).each_with_index do |y, i|
+            rows[y] <<= [(sx - i), (sx + i)]
+        end
+        (sy..(sy + manhat)).each_with_index do |y, i|
+            rows[y] <<= [(sx - (manhat  - i)), (sx + (manhat - i))]
+        end
+    end
+    (0..4000000).each do |n|
+        change = true
+        while change
+            change = false
+            rows[n].each_with_index do |range, ri|
+                rows[n].each_with_index do |subrange, si|
+                    if ri != si
+                        if range[0] <= subrange[0] && range[1] >= subrange[0] && range[1] <= subrange[1]
+                            rows[n][ri] = [range[0], subrange[1]]
+                            rows[n].delete_at(si)
+                            change = true
+                            break
+                        elsif range[0] >= subrange[0] && range[1] <= subrange[1]
+                            rows[n][ri] = [subrange[0], subrange[1]]
+                            rows[n].delete_at(si)
+                            change = true
+                            break
+                        elsif range[1] + 1 == subrange[0]
+                            rows[n][ri] = [range[0], subrange[1]]
+                            rows[n].delete_at(si)
+                            change = true
+                            break
+                        end
+                    end
+                end
+                break if change == true
+            end
+        end
+        return ((rows[n].sort[1][0] - 1) * 4000000) + n if rows[n].length > 1
+    end
 end
 
 pp day15_1
