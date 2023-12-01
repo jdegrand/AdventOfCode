7f8487220b0e07f16cea5e13c4e1fbcb392f66cf
Joe DeGrand <degrandjoe@gmail.com>
2023-11-30 23:20:39 -0800

    Day 1 2023 Ruby

--- 2023/Day1/1.rb
+++ 2023/Day1/1.rb
@@ -25,5 +25,15 @@ def day1_2
     end.sum
 end
 
+def day1_2_alternate
+    overlapping = $nums.permutation(2).to_a.filter{|s1, s2| s1[-1] === s2[0]}.map{|s1, s2| [s1 + s2[1..], s1 + s2]}
+    $lines.map do |l|
+        new_line = l
+        overlapping.each{|over, sep| new_line = new_line.gsub(over, sep)}
+        numbers = new_line.scan(/one|two|three|four|five|six|seven|eight|nine|\d/)
+        [get_num(numbers[0]), numbers.length == 1  ? get_num(numbers[0]) : get_num(numbers[-1]) ].join.to_i
+    end.sum
+end
+
 pp day1_1
 pp day1_2
