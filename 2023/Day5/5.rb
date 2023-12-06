0e89b8a2f5248e0c720841ceeeca4532a8ea0c81
Joe DeGrand <degrandjoe@gmail.com>
2023-12-05 17:27:23 -0800

    Day 5 2023 Part 2 Ruby

--- 2023/Day5/5.rb
+++ 2023/Day5/5.rb
@@ -1,6 +1,4 @@
 require 'set'
-require 'benchmark'
-
 
 file = 'input.txt'
 input = File.read(file)
@@ -32,134 +30,53 @@ def day5_1
     end.min
 end
 
-def day5_3
+def day5_2
     almanac_hash = {}
     seeds = Set.new($lines[0].scan(/\d+/).map(&:to_i))
     seed_ranges = Set.new($lines[0].scan(/\d+/).map(&:to_i))
     sections = %w(seed-to-soil soil-to-fertilizer fertilizer-to-water water-to-light light-to-temperature temperature-to-humidity humidity-to-location)
+    reversed_sections = sections.reverse
     $lines[1..].each do |section|
         mapping_hash = Hash.new{|hash, key| hash[key] = key}
         mapping = section.split("\n")[0].split[0]
         dest, source = mapping.split("-to-")
         section.split("\n")[1..].each do |nums|
             dest_start, source_start, range = nums.scan(/\d+/).map(&:to_i)
-            mapping_hash[[source_start, source_start + range - 1]] = dest_start
+            mapping_hash[[source_start, source_start + range - 1]] = [dest_start, dest_start + range - 1]
         end
         almanac_hash[mapping] = mapping_hash.sort_by { |key| key }.to_h
     end
-    # seeds.map do |seed|
-    #     sections.each do |sec|
-    #         k, (dest_start, range) = almanac_hash[sec].find{|(source_start, source_end), (dest_start, range)| source_start <= seed && seed <= source_end}
-    #         if k
-    #             seed = seed - k.first + dest_start
-    #         end
-    #     end
-    #     seed
-    # end.min
-    pp almanac_hash
-    min_loc = Float::INFINITY
-
-    seed_ranges.each_slice(2).map do |seed_start, seed_range|
-        curr_seed = seed_start
-        while curr_seed < (seed_start + seed_range)
-            seed = curr_seed
-            sections.each do |sec|
-                ss = nil
-                ds = nil
-                # pp almanac_hash[sec]
-                # puts
-                almanac_hash[sec].each do |source, dest_start|
-                    break if source[0] > seed && source[1] > seed
-                    if source[0] <= seed && seed <= source[1]
-                        ss = source[0]
-                        ds = dest_start
-                    end
-                end
-                if ss
-                    seed = seed - ss + ds
-                end
-            end
-            min_loc = [seed, min_loc].min
-            curr_seed += 1
-        end
-        pp "Through #{seed_start}"
-    end
-    min_loc
-end
-
-# def day5_2
-#     almanac_hash = {}
-#     seed_ranges = Set.new($lines[0].scan(/\d+/).map(&:to_i))
-#     sections = %w(seed-to-soil soil-to-fertilizer fertilizer-to-water water-to-light light-to-temperature temperature-to-humidity humidity-to-location)
-#     $lines[1..].each do |section|
-#         mapping_hash = Hash.new{|hash, key| hash[key] = key}
-#         mapping = section.split("\n")[0].split[0]
-#         dest, source = mapping.split("-to-")
-#         section.split("\n")[1..].each do |nums|
-#             dest_start, source_start, range = nums.scan(/\d+/).map(&:to_i)
-#             mapping_hash[(source_start...(source_start + range))] = [dest_start, range]
-#         end
-#         almanac_hash[mapping] = mapping_hash
-#     end
-#     seed_count = 0
-#     seed_ranges.each_slice(2).map do |seed_start, seed_range|
-#         (seed_start...(seed_start + seed_range)).map do |seed|
-#             seed_count += 1
-#             sections.each do |sec|
-#                 k, (dest_start, range) = almanac_hash[sec].find{|k, (dest_start, range)| k.cover?(seed)}
-#                 if k
-#                     seed = seed - k.first + dest_start
-#                 end
-#             end
-#             seed
-#         end.min
-#     end.min
-# end
-
-def day5_2
-    almanac_hash = {}
-    seed_ranges = Set.new($lines[0].scan(/\d+/).map(&:to_i))
-    sections = %w(seed-to-soil soil-to-fertilizer fertilizer-to-water water-to-light light-to-temperature temperature-to-humidity humidity-to-location)
-    $lines[1..].each do |section|
-        mapping_hash = Hash.new{|hash, key| hash[key] = key}
-        mapping = section.split("\n")[0].split[0]
-        dest, source = mapping.split("-to-")
-        section.split("\n")[1..].each do |nums|
-            dest_start, source_start, range = nums.scan(/\d+/).map(&:to_i)
-            mapping_hash[(source_start...(source_start + range))] = [dest_start, range]
-        end
-        almanac_hash[mapping] = mapping_hash
-    end
 
     all_ranges = seed_ranges.each_slice(2).map{|seed_start, seed_range| (seed_start...(seed_start + seed_range))}
-    min_loc = Float::INFINITY
-    # 4020916802
-    time2 = Benchmark.measure {
-    # 108.87645899970084
-    
-    seed_ranges.each_slice(2).map do |seed_start, seed_range|
-        curr_seed = seed_start
-        while curr_seed < (seed_start + seed_range)
-            seed = curr_seed
-            sections.each do |sec|
-                # pp sec
-                k, (dest_start, range) = almanac_hash[sec].find{|k, (dest_start, range)| k.cover?(seed)}
-                if k
-                    seed = seed - k.first + dest_start
+
+    _, max_location = almanac_hash[sections[-1]].values.min_by{|start, _| start}
+    flipped_almanac = almanac_hash.clone
+    sections.each do |s|
+        new_subsection = {}
+        almanac_hash[s].each{|k, v| new_subsection[v] = k}
+        flipped_almanac[s] = new_subsection.sort_by { |key| key }.to_h
+    end
+    loc = 0
+    while true
+        seed = loc
+        reversed_sections.each do |sec|
+            ds = nil
+            ss = nil
+            flipped_almanac[sec].each do |dest, source|
+                break if dest[0] > seed && dest[1] > seed
+                if dest[0] <= seed && seed <= dest[1]
+                    ds = dest[0]
+                    ss = source[0]
                 end
             end
-            min_loc = [seed, min_loc].min
-            curr_seed += 1
+            if ss
+                seed = seed + (ss - ds)
+            end
         end
-        pp "Through #{seed_start}"
+        return loc if all_ranges.any?{ _1.cover?(seed) }
+        loc += 1
     end
-}
-            
-    pp time2.real
-    min_loc
 end
 
-
-
 pp day5_1
-pp day5_3
+pp day5_2
