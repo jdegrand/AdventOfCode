2ce9d44544f440598224b140e06ddd272dfeec84
Joe DeGrand <degrandjoe@gmail.com>
2023-12-12 23:36:37 -0800

    Day 12 2023 Ruby

--- 2023/Day12/12.rb
+++ 2023/Day12/12.rb
@@ -20,29 +20,31 @@ end
 
 # Second Attempt - Using Recursion
 def day12_1_recursion
-    dp = {}
     $lines.map{ |l|
         row, records = l.split
         records = records.split(?,).map(&:to_i)
+        dp = {}
         recursion(0, 0, records.clone, row.clone, dp)
     }.sum
 end
 
-def day12_2
-    # $lines.map{ |l|
-    #     (5..5).map do |n|
-    #         row, records = l.split
-    #         row = ([row] * n).join("?")
-    #         records = records.split(?,).map(&:to_i)
-    #         new_records = []
-    #         n.times{new_records.concat(records)}
-    #         records = new_records
-    #         recursion(0, 0, records.clone, row.clone)
-    #     end
-    # }.flatten.sum
+# Legacy: Worked for examples but not for my input
+def day12_2_legacy
+    $lines.map{ |l|
+        (1..2).map do |n|
+            dp = {}
+            row, records = l.split
+            row = ([row] * n).join("?")
+            records = records.split(?,).map(&:to_i)
+            new_records = []
+            n.times{new_records.concat(records)}
+            records = new_records
+            recursion(0, 0, records.clone, row.clone, dp)
+        end
+    }.map{_1 * ((_2 / _1) ** 4)}.sum
 end
 
-def day12_3
+def day12_2
     n = 5
     $lines.map{ |l|
         row, records = l.split
@@ -56,39 +58,38 @@ def day12_3
     }.sum
 end
 
-#    ?#?#?#?#?#?#?#? 1,3,1,6
 def recursion(index, count, arr, input, dp)
-
-    #  If array is empty and there are still # left, it is not valid
-    return input[index..].chars.any?("#") ? 0 : 1 if arr.empty?
-
-    # Invalid vecause it means there are still elements left in array from above
-    return 0 if index >= input.length
-
-    # -1 means omit this character; there should be a space in between each island
-    return recursion(index + 1, 0, arr, input, dp) if count == -1
-
-    # Incomplete islands are invalid
-    return count > 0 ? 0 : recursion(index + 1, 0, arr, input, dp) if input[index] == ?.
-
+    return dp[[index, count, arr]] if dp.key?([index, count, arr])
     ways = 0
-
-    # Take the path (You have to if it is a #)
-    if count + 1 == arr.first && (index + 1 >= input.length ? true : input[index + 1] != "#")
-        # Path is taken and it completes the island
-        ways += recursion(index + 1, -1, arr[1..], input, dp)
+    #  If array is empty and there are still # left, it is not valid
+    if arr.empty?
+        ways = input[index..].chars.any?("#") ? 0 : 1
+    # Invalid vecause it means there are still elements left in array from above
+    elsif index >= input.length
+        ways = 0 
+    # -1 means omit this character; there should be a space in between each island
+    elsif count == -1
+        ways = recursion(index + 1, 0, arr, input, dp)
+    # Incomplete islands are invalid
+    elsif input[index] == ?.
+        ways = count > 0 ? 0 : recursion(index + 1, 0, arr, input, dp)
     else
-        # Path is taken and it does not complete the island
-        ways += recursion(index + 1, count + 1, arr, input, dp)
-    end
+        # Take the path (You have to if it is a #)
+        if count + 1 == arr.first && (index + 1 >= input.length ? true : input[index + 1] != "#")
+            # Path is taken and it completes the island
+            ways += recursion(index + 1, -1, arr[1..], input, dp)
+        else
+            # Path is taken and it does not complete the island
+            ways += recursion(index + 1, count + 1, arr, input, dp)
+        end
 
-    # Don't take the path (Only if ?)
-    # If count > 0, this means an island is not accounted for and therefore invalid
-    ways += recursion(index + 1, 0, arr, input, dp) if input[index] == "?" if count == 0
+        # Don't take the path (Only if ?)
+        # If count > 0, this means an island is not accounted for and therefore invalid
+        ways += recursion(index + 1, 0, arr, input, dp) if input[index] == "?" if count == 0
+    end
+    dp[[index, count, arr]] = ways
     ways
 end
 
-# pp day12_1
 pp day12_1_recursion
-# pp day12_2
-pp day12_3
+pp day12_2
