8d3d186e39d53c0153c0c79741d45dea0dfd4aeb
Joe DeGrand <degrandjoe@gmail.com>
2023-12-19 15:12:20 -0500

    Day 19 2023 Part 2 Started

--- 2023/Day19/19.rb
+++ 2023/Day19/19.rb
@@ -57,7 +57,6 @@ def recursion(workflow, workflows)
     rules = workflows[workflow]
     resolved_rules = rules.map{ |rule|
         var, sign, num, next_workflow = rule.match(/([a-z]+)(>|<)(\d+):([a-zAR]+)/)&.captures || [nil, nil, nil, rule]
-        pp next_workflow
         if var && sign && num && next_workflow
             "(" + "#{var}#{sign}#{num}&&" + recursion(next_workflow, workflows) + ")"
         else
@@ -65,6 +64,66 @@ def recursion(workflow, workflows)
         end
     }
     return "#{resolved_rules.join("||")}"
+end
+
+def intersect_range(ranges, new_range)
+    ranges.delete((1..4000))
+    # Falls completely between
+    return ranges if ranges.find{ _1.cover?(new_range.min) && _1.cover?(new_range.max) }
+    lower_bound = ranges.find_index{ _1.cover?(new_range.min) }
+    upper_bound = ranges.find_index{ _1.cover?(new_range.max) }
+    
+    # Falls between two ranges
+    if lower_bound && upper_bound
+        return ranges[...lower_bound] + [(ranges[lower_bound].min..ranges[upper_bound].max)].filter{ _1} + (ranges[(upper_bound + 1)..] || [])
+    elsif lower_bound
+        intersected_range = (ranges[lower_bound].min..new_range.max)
+        return ranges[...lower_bound] + [intersected_range] + (ranges[(lower_bound + 1)..] || []).reject{ intersected_range.cover?(_1.max)}
+    elsif upper_bound
+        intersected_range = (new_range.min..ranges[upper_bound].max)
+        return ranges[...upper_bound].reject{ intersected_range.cover?(_1.min)} + [intersected_range] + (ranges[(upper_bound + 1)..] || [])
+    else
+        new_ranges = []
+        ranges.each{|r|
+            new_ranges << r if !(new_range.cover?(r.min) && new_range.cover?(r.max))
+        }
+        # Place in order
+        return (new_ranges + [new_range]).sort_by(&:min)
+    end
+end
+
+# (x<1416&&true)
+def parser(str, index, depth, values)
+    local_string = ''
+    while true
+        curr = str[index]
+        if curr == "("
+            return parser(str, index + 1, depth + 1, values.clone)
+            break
+        elsif curr == ")"
+            return true if local_string == "true"
+            return false if local_string == "false"
+            var = local_string[0]
+            sign = local_string[1]
+            num = local_string[2..].to_i
+
+            new_range = sign == ?< ? (1...num) : ((num + 1)..4000)
+
+            temp = values.clone
+            temp[var] = intersect_range(temp[var], new_range)
+            return temp
+            break
+        elsif curr == "&"
+            return parser(str, index + 2, depth, values)
+            break
+        elsif curr == "|"
+            return parser(str, index + 2, depth, values)
+            break
+        else
+            local_string += curr
+            index += 1
+        end
+    end
 end
 
 def day19_2
@@ -75,9 +134,24 @@ def day19_2
     workflows_array.each { |flow|
         name, rules = flow.match(reg).captures
         workflows[name] = rules.split(?,)
+    } 
+
+    str = recursion("in", workflows)
+    #  Change to 
+
+    values = {
+        ?x => [(1..4000)],
+        ?m => [(1..4000)],
+        ?a => [(1..4000)],
+        ?s => [(1..4000)]
     }
 
-    recursion("in", workflows)
+    "(s<1351&&(a<2006&&(x<1416&&true)||((x>2662&&true)||(false)))||(m>2090&&true)||((s<537&&(a>3333&&false)||(false))||(x>2440&&false)||(true)))||((s>2770&&(s>3448&&true)||((m>1548&&true)||(true)))||(m<1801&&(m>838&&true)||((a>1716&&false)||(true)))||(false))"
+
+    # str.gsub!("||(false)", '')
+    str = "(x<1416&&true)"
+    # intersect_range([4..6, 10..14, 18..21], 2..40)
+    parser(str, 0, 0, values)
 end
 
 pp day19_1
