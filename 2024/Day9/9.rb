f71c38008d050154d89a86d6fd564f794337e8a2
Joe DeGrand <degrandjoe@gmail.com>
2024-12-09 22:21:00 -0800

    Day 9 2024 Part 2

--- 2024/Day9/9.rb
+++ 2024/Day9/9.rb
@@ -39,15 +39,15 @@ end
 
 def day9_2
   fs = {}
-  chunks = Hash.new { |h, k| h[k] = [] }
   nums = []
+  spaces = []
   index = 0
   curr_num = 0
   is_space = false
   $lines[0].chars.each{ |c|
     c = c.to_i
     if is_space
-      chunks[c] += [index]
+      spaces << (index...(index + c))
       (index...(index + c)).each{ |n| fs[n] = 0 }
     else
       nums << [curr_num, (index...(index + c))]
@@ -58,44 +58,31 @@ def day9_2
     is_space = !is_space
   }
 
-  max = chunks.keys.max
   until nums.length == 0
     num, range = nums.pop
     file_size = range.size
 
-    next if max < file_size
+    spaces.each_with_index{ |space, ind|
+      # Only move files to their left 
+      break if space.last > range.first
 
-    # Remove chunk as its not available anymore
-    ind = chunks[max].shift
-    diff = max - file_size
+      if space.size >= file_size
+        # Remove from old spot in file system
+        range.each{ fs[_1] = 0 }
 
-    # Only move backwards, not forwards
-    if ind < range.first
+        # Add to new spot in file syste
+        (space.first...(space.first + file_size)).each{ fs[_1] = num }
 
-      # Add chunk difference to correct spot if it didn't fill entire spacee
-      if diff > 0
-        to_insert_at = chunks[diff].bsearch_index{_1 >= (ind + file_size)} || chunks[diff].length
-        chunks[diff].insert(to_insert_at, ind + file_size)
+        diff = space.size - file_size
+        if diff > 0
+          spaces[ind] = ((space.first + file_size)...space.last)
+        else
+          spaces.delete_at(ind)
+        end
+        break
       end
-
-      # Remove from old spot in file system
-      range.each{ fs[_1] = 0 }
-
-      # Add to new spot in file system
-      (ind...(ind + file_size)).each{ fs[_1] = num }
-    end
-
-    # Lower the max if no more spaces available
-    until chunks[max].length != 0 || max == 0
-      max -= 1
-    end
-
+    }
   end
-  # s = ""
-  # (0...fs.length).each {|v| s += fs[v].to_s}
-  # s
-
-  # 11532524491670 too high
 
   (0...fs.length).reduce(0) {|v, i| v + fs[i] * i}
 
